b6e3c013299c99dce55d8051020a09b995f46226
Matthew J. Thompson <thompsmj@purdue.edu>
2018-07-18 10:08:59 -0400

    Moving awmmpc files

--- tools/sparseGridTools30Aug10/hom4ps2.bat
+++
@@ -1,6 +0,0 @@
-
-@bin\sym2num.exe < %1 > input.num
-
-@move input.num bin\input.num
-
-@bin\flwcrv.exe
--- tools/sparseGridTools30Aug10/plotInterp.m
+++
@@ -1,6 +0,0 @@
-function plotInterp(z)
-
-assert(z.d == 2);
-range = z.range;
-ezsurf(@(x, y) (spinterpLegendre(z, [x, y])), [range(1, 1), range(1, 2), range(2, 1), range(2, 2)]);
-end
